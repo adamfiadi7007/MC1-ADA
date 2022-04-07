3545d27ad3ee9d8b545e28a813a1ec2f739f6e08
Adam Fiadi <adamfiadi@gmail.com>
2022-04-07 23:21:57 +0700

    navigation fix

--- Challenge 1/MainPage/ViewController.swift
+++ Challenge 1/MainPage/ViewController.swift
@@ -15,13 +15,6 @@ struct Movie {
     var movieInfoSeason: String?
 }
 
-struct movieHome {
-    
-    func getMovie() -> [Movie] {
-        return [Movie(movieTitle: "Twenty Five Twenty One", movieDescription: "In a time who dreams seem out of reach, a teen fencer pursues big ambitions and meets a hardworking young man who seeks to rebuild his life", movieInfoYear: "2022", movieInfoRating: "13+", movieInfoSeason: "Season 1")]
-    }
-    
-}
 
 class ViewController: UIViewController {
     
